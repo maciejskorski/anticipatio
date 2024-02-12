361a25d76862c4799e554cfa02bb6a920c424984
xaru <krzysztof.xaru.rajda@gmail.com>
2024-02-12 12:35:50 +0000

    xxx

--- src/xaru/2 calculate_emotions.py
+++ src/xaru/2 calculate_emotions.py
@@ -1,20 +1,9 @@
 import pandas as pd
 from transformers import pipeline
 from pathlib import Path
+import asyncio
 
-repo_path = Path('/home/krajda/anticipatio/')
-
-tweets = pd.read_pickle(repo_path / 'data/final.pkl')
-
-docs = tweets["txt"].tolist()
-
-pipe = pipeline(
-    "text-classification",
-    model="cardiffnlp/twitter-roberta-base-emotion-multilabel-latest",
-    top_k=25,
-)
-
-results = []
+repo_path = Path("/home/krajda/anticipatio/")
 
 empty = [
     {"label": "sadness", "score": 0},
@@ -30,12 +19,40 @@ empty = [
     {"label": "trust", "score": 0},
 ]
 
-for doc in docs:
-    try:
-        r = pipe(doc)[0]
-        results.append(r)
-    except BaseException:
-        results.append(empty)
+pipe = pipeline(
+    "text-classification",
+    model="cardiffnlp/twitter-roberta-base-emotion-multilabel-latest",
+    top_k=25,
+)
 
-df = pd.DataFrame([{d["label"]: d["score"] for d in r} for r in results])
-df.to_pickle(repo_path / "data/emotions.pkl")
+
+async def calculate(txt):
+    try:
+        print()
+        print(txt)
+        x = pipe(txt)[0]
+        print(x)
+        return x
+    except BaseException:
+        return empty
+
+
+async def get_emotions():
+
+    tweets = pd.read_pickle(repo_path / "data/final.pkl")
+
+    docs = tweets["txt"].tolist()
+
+    tasks = [calculate(doc) for doc in docs[3:4]]
+    results = await asyncio.gather(*tasks)
+
+    print(results)
+
+    df = pd.DataFrame([{d["label"]: d["score"] for d in r} for r in results])
+    print(df)
+
+    df.to_pickle(repo_path / "data/emotions.pkl")
+
+
+if __name__ == "__main__":
+    asyncio.run(get_emotions())
